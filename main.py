f73146fa69980a64d322ca30242a0147bc8dc1f7
Lance <austinkarki84@gmail.com>
2025-06-24 16:47:38 +0545

    added options to add image

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from encoder import encode_faces
 from recognizer import FaceRecognizer
+from test import capture_image
 
 if __name__ == "__main__":
-    # First encode faces (run once)
-    encode_faces()
 
-    # Then start real-time recognition
-    recognizer = FaceRecognizer()
-    recognizer.recognize_from_video()
+    # Hmm for now this will work.
+    while True:
+        action = int(input("What do you want to do? \n 1) Add Image \n 2) Detect \n 3) Exit\n"))
+        match action:
+            case 1: 
+                capture_image()
+            case 2: 
+                # First encode faces (run once)
+                encode_faces()
+
+                # Then start real-time recognition
+                recognizer = FaceRecognizer()
+                recognizer.recognize_from_video()
+            case 3: 
+                exit()
--- test.py
+++ test.py
@@ -1,30 +1,31 @@
 import cv2
 
-cap = cv2.VideoCapture(0)  # Open the default camera
+def capture_image():
+    cap = cv2.VideoCapture(0)  # Open the default camera
 
-if not cap.isOpened():
-    print("Error: Could not open webcam.")
-    exit()
+    if not cap.isOpened():
+        print("Error: Could not open webcam.")
+        exit()
 
-print("Press 's' to save image or 'q' to quit.")
+    print("Press 's' to save image or 'q' to quit.")
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Failed to grab frame.")
-        break
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Failed to grab frame.")
+            break
 
-    cv2.imshow("Webcam", frame)
+        cv2.imshow("Webcam", frame)
 
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('s'):
-        image_name = input("Enter Name: ")
-        cv2.imwrite(f"data/{image_name}.jpg", frame)
-        print(f"Image saved as {image_name}.jpg")
-        break
-    elif key == ord('q'):
-        print("Quitting without saving.")
-        break
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('s'):
+            image_name = input("Enter Name: ")
+            cv2.imwrite(f"data/{image_name}.jpg", frame)
+            print(f"Image saved as {image_name}.jpg")
+            break
+        elif key == ord('q'):
+            print("Quitting without saving.")
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
